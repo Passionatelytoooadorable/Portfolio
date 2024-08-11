70f6731d3f371e9a9d8fc228d03b8d622ad816a0
Passionatelytoooadorable <128183101+Passionatelytoooadorable@users.noreply.github.com>
2024-08-11 21:30:21 +0530

    Add files via upload

--- js/education.js
+++ js/education.js
@@ -5,88 +5,67 @@ AOS.init();
 const moocs = document.querySelector(".moocs");
 const moocscards = [
   {
-    title: "ITIL® Foundation Certificate in IT Service Management",
-  /*  cardImage: "assets/images/designs/itil_22", */
-    cardImage: "assets/images/designs/itil_22.png",
-    link: "https://https://www.peoplecert.org/certifications-and-memberships",
+    title: "Postman API Fundamentals Student Expert",
+    cardImage: "images/c9.png",
+    moocLink: "https://api.badgr.io/public/assertions/6kizmTJtTMWRTKbmz6Xs5g",
   },
   {
-    title: "Introduction to Cloud Computing",
-    cardImage: "assets/images/designs/coursera.png",
-    moocLink: "https://www.udemy.com/course/cryptography-for-beginners/",
+    title: "Cloud Computing",
+    cardImage: "images/c1.png",
+    moocLink: "https://onlinecourses.nptel.ac.in/noc21_cs14/preview",
   },
   {
-    title: "HCL Certified Domino Developer Application",
-    cardImage: "assets/images/designs/hcl_domino.png",
-    moocLink: "https://www.coursera.org/learn/machine-learning",
+    title: "Principles of UX/UI Design",
+    cardImage: "images/c2.png",
+    moocLink: "https://coursera.org/verify/L9D6KUFCJAXP",
   },
   {
-    title: "Linkedin Skill Assessment (JavaScript)",
-    cardImage: "assets/images/designs/javascript.png",
-    moocLink: "https://www.linkedin.com/skill-assessments/JavaScript/report/",
-  },
-
- /* {
-    title: "Introduction to CSS 3",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
+    title: "Mastering Data Structures & Algorithms using C and C++",
+    cardImage: "images/c3.png",
+    moocLink: "https://www.udemy.com/certificate/UC-10d27913-91a3-4fa0-ae27-034990653f57/",
   },
   {
-    title: "Javascript",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
+    title: "MATLAB for Data Processing and Visualization",
+    cardImage: "images/c4.png",
+    moocLink: "https://matlabacademy.mathworks.com/details/matlab-for-data-processing-and-visualization/mlvi",
   },
   {
-    title: "Bootstrap 4",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
+    title: "What is Graphic Design?",
+    cardImage: "images/c5.png",
+    moocLink: "https://www.linkedin.com/learning/certificates/ee007c7c29a43ef378d4e3be5e0d14b1ea417f6c0a7fb4e13a84338f11e3e451",
   },
   {
-    title: "Intro to React",
-    cardImage: "assets/images/education-page/edx.svg",
-    moocLink: "https://www.edx.org/learn/reactjs",
+    title: "Vityarthi (Python Essentials)",
+    cardImage: "images/ed1.jpeg",
+    moocLink: "https://vityarthi.com/home/course/python-essentials/2",
   },
   {
-    title: "Intro to React Native",
-    cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink:
-      "https://www.coursera.org/lecture/react-native/introduction-to-react-native-Eax0D",
+    title: "Introduction to C plus plus",
+    cardImage: "images/c6.png",
+    moocLink: "https://simpli-web.app.link/e/QAaE4vIDoJb",
   },
   {
-    title: "NodeJS, Express and MongoDB",
-    cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/learn/server-side-nodejs",
+    title: "IBM AI Engineering",
+    cardImage: "images/c2.png",
+    moocLink: "https://www.coursera.org/account/accomplishments/professional-cert/UF54RZDYBQAB",
   },
   {
-    title: "XML-AJAX",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/xml-from-beginner-to-expert/",
+    title: "AWS Image Processing",
+    cardImage: "images/c7.png",
+    moocLink: "https://olympus.mygreatlearning.com/courses/64212/certificate",
   },
   {
-    title: "Data Structures & Algorithms",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink:
-      "https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256",
-  }, */
+    title: "Machine Learning with Python",
+    cardImage: "images/c2.png",
+    moocLink: "https://coursera.org/verify/Z5K8GS7VNHFW",
+  },
+  {
+    title: "Innovating with Data and Google Cloud",
+    cardImage: "images/c8.png",
+    moocLink: "https://www.cloudskillsboost.google/public_profiles/cc23d13e-19db-4aa6-927d-42eec8c4ad5b/badges/6101998",
+  },
 ];
 
-const experience = [
-  {
-    img: "assets/images/education-page/c1.png",
-  },
-  {
-    img: "assets/images/education-page/c2.jpg",
-  },
-  {
-    img: "assets/images/education-page/c3.png",
-  },
-  {
-    img: "assets/images/education-page/c4.png",
-  },
-  {
-    img: "assets/images/education-page/c5.jpg",
-  },
-];
 
 let currentItem = 0;
 
@@ -135,26 +114,6 @@ const showCards = () => {
 };
 document.addEventListener("DOMContentLoaded", showCards);
 
-/* Badges*/
-
-const bagdes = document.querySelector(".badges");
-const badgesection = [
-  {
-    title: "Google Developer Essentials",
-    image: "assets/images/education-page/badge1.png",
-    description: "Earned May 20, 2020",
-  },
-  {
-    title: "VM Migration",
-    image: "assets/images/education-page/badge2.png",
-    description: "Earned June 20, 2020",
-  },
-  {
-    title: "G Suite Essentials",
-    image: "assets/images/education-page/badge3.png",
-    description: "Earned July 20, 2020",
-  },
-];
 
 const showCards1 = () => {
   let output = "";
